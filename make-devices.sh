34025d6bb921664731408a80dd40d9c18e33ef81
Lars Kellogg-Stedman <lars@redhat.com>
2018-01-27 23:55:27 -0500

    allow input from stdin

--- make-devices.sh
+++ make-devices.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+device_file=${1:-devices.txt}
+
+if [ "$device_file" = "-" ]; then
+	trap 'rm -f $tmpfile' EXIT
+	tmpfile=$(mktemp dataXXXXXX)
+	cat > $tmpfile
+	device_file=$tmpfile
+fi
+
 while read macaddr name; do
 	name="$name" macaddr="$macaddr" ./gurl resource/dhtsensor device.json
-done < devices.txt
+done < $device_file
