b4b9ba3a9c624f979fea744a87faff5da65b3d9c
Lars Kellogg-Stedman <lars@redhat.com>
2018-01-28 10:42:00 -0500

    teach setup.sh how to use different archive policies

--- setup.sh
+++ setup.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+ARCHIVE_POLICY=archive-policy.json
+
+while getopts 'a:' ch; do
+	case $ch in
+		(a)	ARCHIVE_POLICY=$OPTARG
+			;;
+	esac
+done
+shift $((OPTIND - 1))
+
+set -e
+
 ./gurl resource_type resource-type-dhtsensor.json
-./gurl archive_policy archive-policy.json
+./gurl archive_policy $ARCHIVE_POLICY
 ./gurl archive_policy_rule archive-policy-rule.json
